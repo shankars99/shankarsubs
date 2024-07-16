16cab6d210d5fdbdb482f03e4615f8210019053e
shankars99 <shankars0899@gmail.com>
2024-07-16 15:51:28 -0400

    fix: use footer instead of header tag
    Signed-off-by: shankars99 <shankars0899@gmail.com>

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,4 +1,5 @@
 <header>
 	<script id="umaring_js" src="https://umaring.mkr.cx/ring.js?id=theblushirtdude"></script>
 	<div id="umaring" />
+	<img src="https://github.com/umaring/umaring/raw/main/umass.png" alt="UMass Amherst" />
 </header>
